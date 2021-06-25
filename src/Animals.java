cb198575ca284b51907d4ec586f32bd61e7f4d06
dtacademyb5 <86332147+dtacademyb5@users.noreply.github.com>
2021-06-24 21:07:09 -0400

    Update Animals.java

--- src/Animals.java
+++ src/Animals.java
@@ -10,6 +10,9 @@ public class Animals {
         System.out.println("   \"\"    \"\" ");
 
         System.out.println();
+        
+         System.out.println();
+         System.out.println();
     }
 
 
